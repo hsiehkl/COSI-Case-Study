8c2d5021106f27f6dd6353f006c82007a557c7b7
hsiehkl <st4465@gmail.com>
2020-09-16 10:52:21 +0200

    Add link table in multithreading version

--- db_multithreading.py
+++ db_multithreading.py
@@ -44,12 +44,19 @@ class WikiDB:
                         person VARCHAR(255) NOT NULL, 
                         description VARCHAR(255) NULL, 
                         FOREIGN KEY(date_id) REFERENCES date(id))""")
+        self.cursor.execute("""CREATE TABLE link(
+                        id INTEGER NOT NULL PRIMARY KEY,
+                        date_id INTEGER NOT NULL,
+                        title VARCHAR(255) NOT NULL, 
+                        link VARCHAR(255) NULL, 
+                        FOREIGN KEY(date_id) REFERENCES date(id))""")
 
     def drop_wikipedia_tables(self):
         self.cursor.execute("DROP TABLE date")
         self.cursor.execute("DROP TABLE event")
         self.cursor.execute("DROP TABLE birth")
         self.cursor.execute("DROP TABLE death")
+        self.cursor.execute("DROP TABLE link")
 
     def insert_date(self, datetime, year, month, day, date_str):
         params = (datetime, year, month, day, date_str)
@@ -59,7 +66,6 @@ class WikiDB:
         return date_id
 
     def insert_event(self, events):
-        # params = (date_id, event)
         sql_data = tuple((None,) + a for a in events)
         self.cursor.executemany("INSERT INTO event VALUES (?, ?, ?)", sql_data)
         event_id = self.cursor.lastrowid
@@ -67,7 +73,6 @@ class WikiDB:
         return event_id
 
     def insert_birth(self, births):
-        # params = (date_id, person, description)
         sql_data = tuple((None,) + a for a in births)
         self.cursor.executemany("INSERT INTO birth VALUES (?, ?, ?, ?)", sql_data)
         event_id = self.cursor.lastrowid
@@ -75,9 +80,29 @@ class WikiDB:
         return event_id
 
     def insert_death(self, deaths):
-        # params = (date_id, person, description)
         sql_data = tuple((None,) + a for a in deaths)
         self.cursor.executemany("INSERT INTO death VALUES (?, ?, ?, ?)", sql_data)
         event_id = self.cursor.lastrowid
         self.connection.commit()
         return event_id
+
+    def link_exist(self, value):
+        """check if a link already existed"""
+        if value:
+            self.cursor.execute("SELECT title FROM link WHERE title=?", (value, ))
+            result = self.cursor.fetchone()
+            if result:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def insert_link(self, links):
+        for link in links:
+            value = link[1]
+            existed = self.link_exist(value)
+            if not existed:
+                self.cursor.execute("INSERT INTO link VALUES (NULL, ?, ?, ?)", link)
+                event_id = self.cursor.lastrowid
+                self.connection.commit()
--- run_multithreading.py
+++ run_multithreading.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     import datetime
     from concurrent.futures import ThreadPoolExecutor, as_completed
     import time
-    from db_process_pool import WikiDB
+    from db_multithreading import WikiDB
 
     if len(sys.argv) == 2:
 
@@ -41,6 +41,7 @@ if __name__ == '__main__':
             event_list = []
             birth_list = []
             death_list = []
+            link_list = []
 
             response = requests.get(url=url)
             soup = BeautifulSoup(response.content, 'html.parser')
@@ -51,6 +52,7 @@ if __name__ == '__main__':
                     links = li.find_all('a')
 
                     if text[0] == year:
+                        pass
                         if id == "Events":
                             event = text[1]
                             event_list.append((date_id, event))
@@ -70,7 +72,10 @@ if __name__ == '__main__':
                                 birth_list.append((date_id, person, description))
                             elif id == "Deaths":
                                 death_list.append((date_id, person, description))
-            return (event_list, birth_list, death_list)
+
+                        link_data = [(link.get('title'), link.get('href')) for link in links]
+                        [link_list.append((date_id, data[0], data[1])) for data in link_data]
+            return (event_list, birth_list, death_list, link_list)
 
 
         # parese information in mutiple threads for speeding up
@@ -82,6 +87,7 @@ if __name__ == '__main__':
             event_results = []
             birth_results = []
             death_results = []
+            link_results = []
             for result in as_completed(futures):
                 data = result.result()
                 if data[0] != []:
@@ -91,8 +97,11 @@ if __name__ == '__main__':
                 if data[2] != []:
                     [death_results.append(death) for death in data[2]]
 
+                [link_results.append(link) for link in data[3]]
+
             wiki_db.insert_event(event_results)
             wiki_db.insert_birth(birth_results)
             wiki_db.insert_death(death_results)
+            wiki_db.insert_link(link_results)
             end = time.time()
             print("Time Taken: {:.6f}s".format(end-start))
